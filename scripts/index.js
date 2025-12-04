62680e2bbbbd9a5a90a047ef1565f91f818bdd46
Braian Torres <brianntorress.s@gmail.com>
2025-12-04 02:21:11 -0300

    feat(index): change fetch to local server url and add listener to 'project-container'

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import { renderUserInfo, renderProjects } from "../services/indexdomservice.js";
 import { renderTechnologies } from "../services/domservice.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("mock/portfolio.json")
+    fetch("http://127.0.0.1:8000/portfolio")
     .then(response => response.json())
     .then(data => {
 
@@ -13,3 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(error => console.log("Error al obtener datos: ", error));
 })
+
+document.getElementById("project-container").addEventListener("click", (e) =>{
+    let item = e.target.closest(".project-item");
+    if(!item) return;
+
+    let id = item.dataset.projectId;
+    window.location.href = `project.html?id=${id}`;
+});
